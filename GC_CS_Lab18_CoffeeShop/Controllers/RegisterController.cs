12d7aab60f30a6ab15c45282e1ef4de1572e0c4d
Erwin <coronele@hotmail.com>
2020-02-25 00:36:02 -0500

    GC_CS_Lab18_CoffeeShop

---
+++ GC_CS_Lab18_CoffeeShop/Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GC_CS_Lab18_CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GC_CS_Lab18_CoffeeShop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegUser()
+        {
+            return View();
+        }
+
+        /*
+        [HttpPost]
+        public IActionResult RegUser()
+        {
+            return View();
+        }
+        */
+
+        [HttpPost]
+        public IActionResult RegWelcome(RegisterUser registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(registerUser);
+            }
+            else
+            {
+                return View("RegUser", registerUser);
+            }
+        }
+    }
+}
---
+++ GC_CS_Lab18_CoffeeShop/Models/RegisterUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_CS_Lab18_CoffeeShop.Models
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage ="Username is required")]
+        [StringLength(100,MinimumLength = 8)]
+        public string UserName { get; set; }
+        [RegularExpression(@"\b.{2,}@.{2,}\..{2,3}\b")]
+        public string Email { get; set; }
+        public string EmailType { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+        public int GCCoffeeLove { get; set; }
+
+        public RegisterUser() { }
+        public RegisterUser(string username, string email, string emailtype, string pw, int coffeelove)
+        {
+            UserName = username;
+            Email = email;
+            Password = pw;
+            EmailType = emailtype;
+            GCCoffeeLove = coffeelove;
+        }
+    }
+}
